fa1c4e1bf2b030acab2f27db2750496aaa8e7315
Amarilio de Oliveira <amarilioalencar@gmail.com>
2025-07-17 21:20:28 +0100

    feat: organize imports with path mappings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@/config": path.resolve(__dirname, "./src/config"),
+      "@/lib": path.resolve(__dirname, "./src/lib"),
+      "@/services": path.resolve(__dirname, "./src/services"),
+      "@/types": path.resolve(__dirname, "./src/types"),
+      "@/utils": path.resolve(__dirname, "./src/utils"),
+    },
+  },
   server: {
     port: 8080,
     open: true,
